8fa5f34c688faa7d9d5be179355333ae8c3ebb1c
Anshu <anshusingh@192.168.1.8>
2024-09-30 12:32:03 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { memo } from 'react';
+
+/**
+ * App Footer
+ *
+ * @example
+ * <Footer />
+ */
+function Footer() {
+  return (
+    <footer className="footer">
+      Built with ❤
+    </footer>
+  );
+}
+export default memo(Footer);
