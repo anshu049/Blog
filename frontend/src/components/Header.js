938bc3864c3c84303158bcfaaf44ee99b6233655
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-09-30 13:54:40 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -96,11 +96,6 @@ function Header() {
     <nav className="navbar navbar-light">
       <div className="container">
         <Link to="/" className="navbar-brand">
-          <span className="nav-logo">
-            c{'{'}
-            {<span style={{ color: '#FB86B6' }}>api</span>}
-            {'}'}tal
-          </span>
           {
             //appName.toLowerCase()
           }
